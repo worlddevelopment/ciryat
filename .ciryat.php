8dacf256c245fc761699b82ef7183f79daf884ea
faerietree <radagast@ciry.at>
2017-07-05 21:35:21 +0200

    Add .ciryat.php HTM base.

---
+++ .ciryat.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="A world development open eco club with board ethics instead of hierarchies." />
+	<meta name="keywords" content="free, open, economy, ecology, technology, machines, bots, security" />
+	<meta name="audience" content="All" />
+	<meta name="author" content="worlddevelopment" />
+	<meta name="publisher" content="worlddevelopment" />
+	<meta name="Robots" content="index,follow" />
+	<meta name="revisit-after" content="1 Day" />
+	<meta name="Language" content="English" />
+	<meta name="Content-Language" content="en" />
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="stylesheet" href="./css/style.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="./css/fairyclasses.css" type="text/css" media="screen">
+	<style>
+	/*<![CDATA[*/
+
+	/*]]>*/
+	</style>
+	<title>worlddevelopment</title>
+</head>
+<body>
+
+<!-- WRAPPER -->
+<div id="wrapper">
+
+<?php
+require_once("./body.tpl.php");
+?>
+
+
+</div><!-- Wrapper -End -->
+
+
+<!-- fixed position elements: -->
+<div class="vertical_bar">
+</div>
+
+<script type="text/javascript" src="./js/site.js"></script>
+<script type="text/javascript" src="./js/title-animation.js"></script>
+
+</body></html>
+
