86e3edb73c0ba9ebff9d999da67f88874a4fc724
faerietree <radagast@ciry.at>
2017-08-05 10:03:32 +0200

    Reduce menu origin to element radius length further to better fit
    on a lot wider than high window sizes (extreme landscape format).

--- dynamicmenu.config.php
+++ dynamicmenu.config.php
@@ -181,7 +181,7 @@ $path = './';
 $type = 'allget'; # {all|files|dir}get <=> what to phpInclude
 $circular = true;
 $origin = ['38%', '48%'];
-$radius = 27;
+$radius = 25;
 $unit = '%';
 
 $evolved = true;
