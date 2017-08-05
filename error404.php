cd74ad9294cb55f5a566d9ca2fa1838f7ad937df
faerietree <radagast@ciry.at>
2017-08-05 17:37:16 +0200

    Add an error404.php file.

---
+++ error404.php
@@ -0,0 +1,12 @@
+<div class="container">
+
+
+    <h1>Error 404</h1>
+    <h2>Page not found | Seite nicht gefunden!</h2>
+	<p></p>
+    <p>Bei Fragen, Anregungen oder W&uuml;nschen kontaktieren Sie uns
+    doch einfach per Email, die Sie unseren Github Profilen entnehmen k&ouml;nnen.
+    </p>
+
+
+</div>
