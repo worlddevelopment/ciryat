78b7ec30313663dbe2d2c54d63dcded5aa41c826
faerietree <radagast@ciry.at>
2017-08-06 15:58:01 +0200

    Rename functions.inc.php to .functions.inc.php for consistency.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 
 <?php
-include_once("functions.inc.php");
+include_once(".functions.inc.php");
 /* The content is kept in a file with more specific name as index.php exists mutiple times and overwriting then were fatal. */
 include(".ciryat.php");
 ?>
