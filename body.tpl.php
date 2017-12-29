c610e66ea3b5da3e65fb0098e95eb5a4b06c766f
faerietree <radagast@ciry.at>
2017-12-29 17:26:33 +0000

    Use human readable ko-fi.com/openecoclub

--- body.tpl.php
+++ body.tpl.php
@@ -26,7 +26,7 @@
 		</ul>
 		<ul style="position: fixed; bottom: -.5rem; left: 1rem">
 <li>
-<a href='https://ko-fi.com/P5P86Q30' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://az743702.vo.msecnd.net/cdn/kofi2.png?v=0' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
+<a href='https://ko-fi.com/openecoclub' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://az743702.vo.msecnd.net/cdn/kofi2.png?v=0' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
 </li>
 			<li>
 		</ul>
