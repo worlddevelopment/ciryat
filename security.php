9fe11a48f7214bedd61d9ee390c3924227cebb26
faerietree (ciryat) <radagast@ciry.at>
2018-04-16 09:54:51 +0000

    Add security

---
+++ security.php
@@ -0,0 +1,75 @@
+
+	<!-- topmost bar | sub nav -->
+	<div id="thumbHolder">
+		<!--
+		<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+		-->
+		<a href="http://ciry.at">
+			<div id="mini-logo" class="logo">
+				<span>worlddevelopment</span>
+			</div>
+		</a>
+		<ul class="nav top-nav nav-collapse collapse" id="nav-menu">
+			<li id="menu-item-home" class="menu-item active">
+				<label for="harbor">
+					<img src="" alt=""/>
+					&#x21E1; Harbor
+				</label>
+			</li><li class="menu-item active">
+				<label for="2">
+					<img src="" alt=""/>
+					Prevention
+				</label>
+			</li><li class="menu-item active">
+				<label for="1">
+					<img src="" alt=""/>
+					Search &amp; Rescue
+				</label>
+			</li>
+		</ul>
+	</div>
+
+
+	<!-- main content -->
+	<form id="fullHolder">
+
+
+<input type="radio" id="1" name="nav"/>
+<section>
+	<div class="container">
+		<h1>Search &amp; Rescue</h1>
+		<div>
+Virtually all worlddevelopment machines can help here.
+		</div>
+	</div>
+</section>
+
+
+<input type="radio" id="2" name="nav"/>
+<section>
+	<div class="container">
+		<h1>Prevention</h1>
+		<div>
+		<h2>The foremost priority</h2>
+It is not forecast when, what happens. Instead the principle is availability of good, trusted powers.
+		<h2>How exactly shall that work?</h2>
+Combination of trusted cyber and real bot nets. Show presence. Deter. Be there when you are needed like a wizard. Preparedness.
+		</div>
+	</div>
+</section>
+
+<style type="text/css">
+form#fullHolder section:nth-of-type(3)
+{
+	background-image: url("img/schildmetallskyblue.png");
+}
+</style>
+<?php include(".harbor.php") ?>
+
+
+	</form><!-- main content -End -->
+
