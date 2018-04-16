ce3ba07d6a72d7d2b227bb64a7a24f073ce6a934
faerietree (ciryat) <radagast@ciry.at>
2018-04-16 09:59:06 +0000

    Add home

---
+++ home.php
@@ -0,0 +1,166 @@
+
+	<!-- topmost bar | sub nav -->
+	<div id="thumbHolder">
+		<!--
+		<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+		-->
+		<a href="http://ciry.at">
+			<div id="mini-logo" class="logo">
+				<span>worlddevelopment</span>
+			</div>
+		</a>
+		<ul class="nav top-nav nav-collapse collapse" id="nav-menu">
+			<li id="menu-item-home" class="menu-item active">
+				<label for="harbor">
+					<img src="" alt=""/>
+					&#x21E1; Harbor
+				</label>
+			</li><li class="menu-item active">
+				<label for="news">
+					<img src="" alt=""/>
+					News
+				</label>
+			</li><li class="menu-item active">
+				<label for="contact">
+					<img src="" alt=""/>
+					Contact
+				</label>
+			</li>
+		</ul>
+	</div>
+
+
+	<!-- main content -->
+	<form id="fullHolder">
+
+
+<?php include(".contact.php") ?>
+
+
+<input type="radio" id="news" name="nav"/>
+<section>
+	<div class="container">
+		<h1>News</h1>
+		<div class="overflowy">
+		<div style="box-shadow: 1px 1px;">
+		<p><span>2018-04-20</span> Machines: Design autonomous blue lawn mower @ <200€</p>
+		<p><span>2018-04-15</span> Web: Standardization on Composer + Git overlay</p>
+		<p><span>2018-04-15</span> Web: Standardization on Symfony|Laravel, Flow|Neos, Aimeos</p>
+		<p><span>2018-04-14</span> Riverbridge: Started floor heating construction @ level 1</p>
+		<p><span>2018-04-14</span> Riverbridge: Painted last remaining white wall @ level 1</p>
+		<p><span>2018-04-11</span> Equestrian farm: Plastered westernmost wall of horse stables</p>
+		<p><span>2018-04-01</span> Riverbridge: New contract at conditions that benefit both sides significantly</p>
+		</div>
+		<div style="box-shadow: 1px 1px;">
+		<p><span>2017-12-19</span> Virtual time machine: Fine tune timing of breakthrough</p>
+		<p><span>2017-12-18</span> Virtual time travel breakthrough</p>
+		<p><span>2017-12</span> Horses: Earth, concrete works for new riding ground</p>
+		</div>
+		<div style="box-shadow: 1px 1px;">
+		<p><span>2017-12-01</span> Shift time from dead HannibalAI to alive HybridAI</p>
+		<p><span>2017-11-30</span> Water level alarm system success (expected 6V but got 4..7VAC with sign alternating DC component -3.6..3.8 put positive side to use for triggering alarm as AC signal alarm raised only by 1VAC and proved unreliable)</p>
+		</div>
+		<div style="box-shadow: 1px 1px;">
+		<p><span>2017-11-25</span> 3D printing success (needed to adapt to colder temperatures approx. 5 degrees celsius)</p>
+		<p><span>2017-11-24</span> Used interwinter weather opportunity to finish western hobbit wall + garden path 1/3</p>
+		<p><span>2017-11-19</span> Beagle bone blue shines, interbot communications success</p>
+		</div>
+		<div style="box-shadow: 1px 1px;">
+		<p><span>2017-11-14</span> Communications module attached to BeagleBones in an as compact, simple as possible way (requires 10 min)</p>
+		<p><span>2017-11-14</span> Mailserver working</p>
+		<p><span>2017-11-13</span> All websites up and running</p>
+		<p><span>2017-11-10</span> Mitigating hacker attacks: New shiny safer, simpler, up to date server</p>
+		<p><span>2017-11-08</span> Water proof level 1 material ramp</p>
+		</div>
+		<div style="box-shadow: 1px 1px;">
+		<p><span>2017-11-05</span> Mini roof walls north east. Rush lay planks on level one harvest entry platform (during night) and protect it using tar before heavy rain arrives at dawn (vampire action).</p>
+		<p><span>2017-11-04</span> Create mini hobbit roof walls for earth and plants in south west. Create harvest entry platform between hobbit storehouse and riverbridge castle.</p>
+		</div>
+		<div style="box-shadow: 1px 1px;">
+		<p><span>2017-11-03</span> Order all logic, comm parts for Walker, Natural heater, Manipulator, Burgau comm, HAMAG bender, Golf cart</p>
+		<p><span>2017-11-02</span> Design hydro power plant Burgau comm, minimalistic scalable DC motor speed controller (Golf cart).</p>
+		<p><span>2017-11-01</span> Think a component selection strategy ensuring modularity for all projects (HAMAG, worlddevelopment, ...)</p>
+		</div>
+		<div style="box-shadow: 1px 1px;">
+		<div style="box-shadow: 1px 1px;">
+		<p><span>2017-10-31</span> Established connection between riverbridge and hilltop</p>
+		</div>
+		<div style="box-shadow: 1px 1px;">
+		<p><span>2017-10-28</span> Finished hobbit ceiling. Closed roof. Did withstand latest storm. :-)</p>
+		</div>
+		<div style="box-shadow: 1px 1px;">
+		<p><span>2017-10-25</span> Finished last 2 hobbit, tikal windows</p>
+		<p><span>2017-10-24</span> Finished steel, wood floor, hatch, construction above cellar (tiles missing)</p>
+		<p><span>2017-10-23</span> Oiled, laid wooden bars on top of hobbit roof</p>
+		</div>
+		<p><span>2017-10-22</span> Finished hobbit hole steel roof construction</p>
+		<p><span>2017-10-21</span> Fetched wooden bars .2 * .1. Ground bars for roof and entry to riverbridge base level 1 (remember old stairs got removed).</p>
+		<p><span>2017-10-21</span> Fetched another pallet of bricks for hobbits</p>
+		<p><span>2017-10-21</span> Cleared border, laid natural stones. Damaged parts of the eagle terraces</p>
+		<p><span>2017-10-20</span> Finished major groundwork for vortex stairs (all but last step, needs additional steel I beam of length 2.85).</p>
+		<p><span>2017-10-19</span> Surrected roof, spiral stairs at north west frontier</p>
+		<p><span>2017-10-19</span> Redesigned, executed roof construction one last time, now flat, yet more hobbit-like. Room for additional terraces, maybe stone bakery oven. Redesign frees half of the bought metal for walker (400 Kg)</p>
+		<p><span>2017-10-16</span> Redesigned roof another time. A nice natural fire place on top of the hill for epic future throngs</p>
+		<p><span>2017-10-16</span> Removed the second fence and laid ground for the helix stairs (Bauantrag pending). Poured north western hobbit wall|stairs foundation and laid more bricks.</p>
+		<p><span>2017-10-15</span> Poured concrete at repurposed metal fence (rebar) for hobbit roof</p>
+		<p><span>2017-10-15</span> Collected leaves, put them to a nice natural place in the forest (legally)</p>
+		<p><span>2017-10-14</span> Inserted stainless steel chimney inserts into cellar</p>
+		<p><span>2017-10-14</span> Started border rework at hobbit hole contact edge. Removed fence, laid bricks.</p>
+		<p><span>2017-10-13</span> Continued hobbit hole walls. Second hobbit window. Built hobbit 1.8m diameter door arc.</p>
+		<p><span>2017-10-12</span> Continued northern hobbit hole wall. First hobbit window.</p>
+		<p><span>2017-10-12</span> Got, unloaded, zinc painted 3 steel I beams for solar holder, hobbit roof.</p>
+		<p><span>2017-10-12</span> Redesigned hobbit roof. Cost reduction by 50%.</p>
+
+		<p><span>2017-10-11</span> Solved details of the solar panel holder that integrates with the eagle terraces and the hobbit hole.</p>
+		<p><span>2017-10-11</span> Designed new level 1 outside entry including natural stairs, fancy siding.</p>
+		<p><span>2017-10-11</span> Determined height slope of hobbit hole walls to be able to lay 6 more layers of bricks (average).</p>
+		<p><span>2017-10-11</span> Measured green hobbit hole roof. Crazy construction, every steel beam different length and angle. 3D model proved difficult to get accurate. So real world measurement it was.</p>
+		<p><span>2017-10-10</span> Fixed egyptian bakery oven (again). Lights do not withstand the heat for longer than 2 weeks it seems.</p>
+		<p><span>2017-10-10</span> Brought and shoveled concrete gravel sand mix to riverbridge western front and heap for ground light stones at northern hobbit wall.</p>
+		<p><span>2017-10-10</span> Finished horse stables rework. Tidied up.</p>
+		<p><span>2017-10-9</span> Fetched 3t of new bricks for shed, border rework. Distance 20Km</p>
+		<p><span>2017-10-8</span> Walker base settled, parts specified</p>
+		<p><span>2017-10-7</span> Extended brick chimney into the cellar. Cut slots for metal floor support. (no more concrete saw dust in level 0 from now on :-))</p>
+		<p><span>2017-10-6</span> Finished removing concrete stairs level 0.</p>
+		<p><span>2017-10-4</span> Started removing concrete stairs level 0 (level 1, 2 wood stairs have been removed last year)</p>
+		<p><span>2017-10-4</span> Remove leftovers from times of wilderness at westernmost fence.</p>
+		<p><span>2017-10-3</span> Finished repair of westernmost foundation by shoveling tons of gravel</p>
+		<p><span>2017-10-3</span> Started horse stables rework</p>
+		<p><span>2017-10-2</span> Filled up at southern hobbit hole wall with rough gravel from `Forggensee`</p>
+		<p><span>2017-10-1</span> Plastered southern wall of hobbit hole. Waterproofed it.</p>
+		<p><span>2017-10-1</span> Dug out southern hobbit hole wall base</p>
+		<p><span>2017-9-30</span> Finished eagle terraces</p>
+		<p><span>2017-9</span> Laid empty pipes for electricity, water.</p>
+		<p><span>2017-9</span> Constructed extra foundation at west side of hobbit hole shed</p>
+		<p><span>2017-9</span> Laid horse proven bricks, natural stones around hobbit hole shed</p>
+		<p><span>2017-9</span> Natural stone circle laid around grass cirle at position of worlddevelopment logo's gear planet</p>
+		<p><span>2017-9</span> Grow first brick walls of hobbit hole.</p>
+		<p><span>2017-9</span> Started eagle terraces</p>
+		<p><span>2017-8</span> Dug out 30 m trench for water drainage from shed, gutters, a possible future car port</p>
+		<p><span>2017-8</span> Dug out foundations. Poured concrete.</p>
+		<p><span>2017-8</span> Aligned foundations, gate, circular grass, flower bed.</p>
+		<p><span>2017-8</span> Started hobbit hole shed series</p>
+		<p><span>2017-8</span> Rebuilt westernmost foundation. Fixes structural integrity</p>
+		<p><span>2017-7</span> Finished removing auxiliary stairs</p>
+		<p><span>2017-spring</span> Finished gate system metal works</p>
+		<p><span>2017-spring</span> Finished brick roundabout</p>
+		</div>
+	</div>
+</section>
+
+
+<style type="text/css">
+form#fullHolder section:nth-of-type(3)
+{
+	background-image: url(".images/worlddevelopment_inverted_white_only.png");
+}
+</style>
+<?php include(".harbor.php") ?>
+
+
+	</form><!-- main content -End -->
+
