3e6fb3a3d75c71d4680d5a0353f64579ea41c4f1
faerietree (ciryat) <radagast@ciry.at>
2018-10-18 12:01:12 +0000

    Unify index.php simplifying everything,e.g. improving single source

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
-
 <?php
-include_once("./.functions.inc.php");
-include_once("./dynamic_menu/dynamicmenu.config.php");
+//echo __FILE__;
+$target = dirname(__FILE__);
+if (is_link(__FILE__))
+	$target = readlink(__FILE__);
+//echo $target;
+include_once($target . "/.functions.inc.php");
+include_once($target . "/dynamic_menu/dynamicmenu.config.php");
 include_once("./dynamicmenu.config.php");
-
-/* The content is kept in a file with more specific name as index.php exists mutiple times and overwriting then were fatal. */
-include("./.ciryat.php");
+include("./.site.php");
 ?>
